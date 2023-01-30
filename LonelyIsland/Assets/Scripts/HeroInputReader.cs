cc68c463791f0c6bc0e313a242b6dd27cd37db51
MOADproglep <62516414+MOADproglep@users.noreply.github.com>
2023-01-31 01:41:45 +0500

    Add Button and Joystick

---
+++ LonelyIsland/Assets/Scripts/HeroInputReader.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Assets.Scripts
+{
+    public class HeroInputReader : MonoBehaviour
+    {
+        [SerializeField] private Hero _hero;
+
+        public void OnMovementAction(InputAction.CallbackContext context)
+        {
+            var direction = context.ReadValue<Vector2>();
+            _hero.SetDirection(direction);
+        }
+    }
+}
